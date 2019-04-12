fec0763a78ba45e1ab4c5ce0d111f4661172c28b
daniel.kolek <daniel.kolek@rohlik.cz>
2019-04-13 00:38:54 +0200

    Pretty playable version; Score tracker; Restart with space

--- src/com/company/board/Board.java
+++ src/com/company/board/Board.java
@@ -20,14 +20,19 @@ public class Board extends JPanel implements KeyListener, ActionListener {
     private static final int BOARD_WIDTH = TILE_SIZE * 13;
     private static final int MAX_TILES_IN_A_ROW = BOARD_WIDTH/TILE_SIZE;
 
+    // Kind of ez - val=10, freq=20, wgen=24, eachTickTileGoDownBy=TILE_SIZE / 4
+
+    // Nice & smooth - init_delay=50, min_delay=15, val=10, freq=20, wgen=48, eachTickTileGoDownBy=TILE_SIZE / 8
+
     private static final int INITIAL_SPEED_INCREASE_FREQUENCY = 20;
     private static final int INITIAL_SPEED_INCREASE_VALUE = 10;
-    public static final int INITIAL_WALL_GENERATION_FREQUENCY = 24;
+    private static final int INITIAL_WALL_GENERATION_FREQUENCY = 48;
+    private static final int INITIAL_DELAY = 50;
+    private static final int MIN_DELAY = 15;
 
-    private static int SPEED_INCREASE_VALUE = INITIAL_SPEED_INCREASE_VALUE;
-    private static int SPEED_INCREASE_FREQUENCY = INITIAL_SPEED_INCREASE_FREQUENCY;
-    private static int WALL_GENERATION_FREQUENCY = INITIAL_WALL_GENERATION_FREQUENCY;
-    private static int DELAY = 70;
+    private static int SPEED_INCREASE_VALUE;
+    private static int SPEED_INCREASE_FREQUENCY;
+    private static int WALL_GENERATION_FREQUENCY;
 
     private static boolean gameRunning;
 
@@ -37,35 +42,47 @@ public class Board extends JPanel implements KeyListener, ActionListener {
     private int posX;
     private int posY;
 
-    private List<WallLine> listOfWallLists = new ArrayList<>();
+    private List<WallLine> listOfWallLists;
 
     private Image player;
     private Image wall;
 
+    Font font;
+    FontMetrics metrics;
     Random random;
     Timer timer;
 
     public Board() {
         addKeyListener(this);
 
+        font = new Font("Helvetica", Font.BOLD, 18);
+        metrics = getFontMetrics(font);
+
         setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
         setFocusable(true);
         setBackground(new Color(238,238,238));
-//        setBackground(new Color(0,0,0));
+        setFont(font);
 
         loadImages();
         initGame();
     }
 
     private void initGame() {
+        random = new Random();
+
+        SPEED_INCREASE_VALUE = INITIAL_SPEED_INCREASE_VALUE;
+        SPEED_INCREASE_FREQUENCY = INITIAL_SPEED_INCREASE_FREQUENCY;
+        WALL_GENERATION_FREQUENCY = INITIAL_WALL_GENERATION_FREQUENCY;
+
         posX = (BOARD_WIDTH / 2) - (TILE_SIZE / 2);
         posY = BOARD_HEIGHT - (TILE_SIZE * 4);
 
-        random = new Random();
+        listOfWallLists = new ArrayList<>();
 
+        SCORE_COUNT = 0;
         TICK_COUNT = 0;
         gameRunning = true;
-        timer = new Timer(DELAY, this);
+        timer = new Timer(INITIAL_DELAY, this);
         timer.start();
     }
 
@@ -106,6 +123,9 @@ public class Board extends JPanel implements KeyListener, ActionListener {
                 }
             }
 
+            graphics.drawString(String.valueOf(SCORE_COUNT), (TILE_SIZE / 4), (BOARD_HEIGHT) - 6);
+
+
             Toolkit.getDefaultToolkit().sync();
 
         } else {
@@ -116,13 +136,18 @@ public class Board extends JPanel implements KeyListener, ActionListener {
     public void gameOver(Graphics g) {
         String msg = "Game Over";
         String score = "Score: " + SCORE_COUNT;
-                Font font = new Font("Helvetica", Font.BOLD, 18);
-        FontMetrics metrics = getFontMetrics(font);
 
         g.setColor(Color.black);
         g.setFont(font);
         g.drawString(msg, (BOARD_WIDTH - metrics.stringWidth(msg)) / 2, BOARD_HEIGHT / 2);
         g.drawString(score, (BOARD_WIDTH - metrics.stringWidth(score)) / 2, (BOARD_HEIGHT / 2) + TILE_SIZE);
+    }
+
+    public void restart() {
+        removeAll();
+        initGame();
+        revalidate();
+        repaint();
     }
 
     @Override
@@ -156,7 +181,7 @@ public class Board extends JPanel implements KeyListener, ActionListener {
             if (currentWallLine.getPosY() + TILE_SIZE > BOARD_HEIGHT) {
                 iterator.remove();
             } else {
-                currentWallLine.setPosY(currentWallLine.getPosY() + TILE_SIZE/4);
+                currentWallLine.setPosY(currentWallLine.getPosY() + TILE_SIZE/8);
             }
         }
 
@@ -166,7 +191,7 @@ public class Board extends JPanel implements KeyListener, ActionListener {
 
 //        updateSpeedChangeFrequency();
 
-        if(TICK_COUNT > SPEED_INCREASE_FREQUENCY && timer.getDelay() > 20) {
+        if(TICK_COUNT > SPEED_INCREASE_FREQUENCY && timer.getDelay() > MIN_DELAY) {
             timer.setDelay(timer.getDelay() - SPEED_INCREASE_VALUE);
             SPEED_INCREASE_FREQUENCY += INITIAL_SPEED_INCREASE_FREQUENCY;
             TICK_COUNT = 0;
@@ -209,6 +234,10 @@ public class Board extends JPanel implements KeyListener, ActionListener {
 
         if (key == KeyEvent.VK_RIGHT && posX < BOARD_WIDTH - TILE_SIZE && !isThereAWallHere(posX + TILE_SIZE)) {
             posX += TILE_SIZE;
+        }
+
+        if (!gameRunning && key == KeyEvent.VK_SPACE) {
+            restart();
         }
 
         if (key == KeyEvent.VK_DOWN) {
